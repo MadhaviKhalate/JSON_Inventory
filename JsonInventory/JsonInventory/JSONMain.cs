29cb4663295941340c6ccd7bf3308b18c9dcc66a
MadhaviKhalate <khalatemadhavi1610@gmail.com>
2022-05-27 10:45:18 +0530

    [Madhavi] added json inventory of rice pulses and wheats

---
+++ JsonInventory/JsonInventory/JSONMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace JsonInventory
+{
+    internal class JSONMain
+    {
+        JsonModel JsonModel = new JsonModel();
+        List<JsonModel> InventoryList = new List<JsonModel>();
+
+        public void Convert(string JFilePath)
+        {
+            using (StreamReader reader = new StreamReader(JFilePath))
+            {
+                var json = reader.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<JsonModel>>(json);
+                Console.WriteLine("Item     Price     Weight   Total Value");
+
+                foreach (var objects in items)
+                {
+                    double value = objects.price * objects.weight;
+                    Console.WriteLine(objects.name + " \t " + objects.price + " \t " + objects.weight 
+                        +" \t " + value);
+                }
+            }
+        }
+    }
+}
---
+++ JsonInventory/JsonInventory/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JsonInventory;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        JsonInventory.JSONMain json = new JsonInventory.JSONMain();
+        json.Convert(@"D:\BridgeLabzFelloship\JSON_Inventory\JsonInventory\JsonInventory\JsonInventory.json");
+    }
+}
